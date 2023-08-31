f220c74fdcdd16ad77921be3897aecbd2e2309eb
Javier Notebook <j.alcalde01@ufromail.cl>
2023-08-31 11:20:08 -0400

    Commit 2 Javier Alcalde

--- src/main/java/HolaMundo.java
+++ src/main/java/HolaMundo.java
@@ -2,8 +2,6 @@ public class HolaMundo {
 // Javier Alcalde
     public static void main(String[] args) {
 
-        System.out.println("Hola Mundo");
-
+        System.out.println("Hola Mundo de Javier Alcalde");
     }
-
 }
