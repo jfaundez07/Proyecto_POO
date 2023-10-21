3cda4629e2b2102a4046bc3919e5d62d1d2d4ea4
jfaundez07 <j.faundez07@ufromail.cl>
2023-10-21 16:13:35 -0300

    Modificaciones Class Cliente

--- src/main/java/SegundaVersion/Cliente.java
+++ src/main/java/SegundaVersion/Cliente.java
@@ -1,15 +1,21 @@
 package SegundaVersion;
 
 public class Cliente {
+
+    //Atributros:
     private String usuario;
     private String contraseña;
     private int celular;
+
+    //Constructores:
 
     public Cliente(String usuario, String contraseña, int celular) {
         this.usuario = usuario;
         this.contraseña = contraseña;
         this.celular = celular;
     }
+
+    //Metodos getter:
 
     public String getUsuario() {
         return usuario;
@@ -19,17 +25,18 @@ public class Cliente {
         return celular;
     }
 
+    //Metodos setter:
+
     public void setUsuario(String usuario) {
         this.usuario = usuario;
-    }
-
-    public void setContraseña(String contraseña) {
-        this.contraseña = contraseña;
     }
 
     public void setCelular(int celular) {
         this.celular = celular;
     }
 
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
 
 }
