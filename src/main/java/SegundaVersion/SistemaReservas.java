423059069373d029f006b498e48dc1ee516644cf
jfaundez07 <j.faundez07@ufromail.cl>
2023-10-21 16:49:28 -0300

    Enviar actualizaciones

--- src/main/java/SegundaVersion/SistemaReservas.java
+++ src/main/java/SegundaVersion/SistemaReservas.java
@@ -6,9 +6,12 @@ import java.util.ArrayList;
 
 public class SistemaReservas {
 
-    //Listas para almacenar los datos de los usuarios y las cabañas en formato de sus respectivas Clases:
+    //Listas para almacenar los datos en formato de sus respectivas Clases:
+
     private ArrayList<Cliente> listaClientes = new ArrayList<>();
     private ArrayList<Cabaña> listaCabañas = new ArrayList<>();
+
+    //Metodos para instanciar los objetos, a partir de un Json:
 
    private Cliente instanciarCliente (JSONObject archivoCliente) {
        return new Cliente(archivoCliente.getString("Usuario"), archivoCliente.getString("Contraseña"), archivoCliente.getInt("Celular"));
@@ -17,6 +20,8 @@ public class SistemaReservas {
    private Cabaña instanciarCabaña (JSONObject archivoCabaña) {
        return new Cabaña(archivoCabaña.getInt("Id"), archivoCabaña.getString("Nombre"), archivoCabaña.getInt("Habitaciones"), archivoCabaña.getInt("Baños"), archivoCabaña.getBoolean("isOcupada"), archivoCabaña.getInt("Arrendatario"));
    }
+
+   //Metodos para almacenar el objeto en la lista de objetos:
 
    private void almacenarCliente(Cliente nuevoCliente){
        this.listaClientes.add(nuevoCliente);
