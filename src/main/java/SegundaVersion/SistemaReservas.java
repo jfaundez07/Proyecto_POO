5966c908b7f80c5f5159f5f28342a4eeaea3eb8a
jfaundez07 <j.faundez07@ufromail.cl>
2023-10-21 16:32:59 -0300

    Nueva Clase SistemaReservas

---
+++ src/main/java/SegundaVersion/SistemaReservas.java
@@ -0,0 +1,9 @@
+package SegundaVersion;
+
+import org.json.JSONObject;
+import java.util.ArrayList;
+
+public class SistemaReservas {
+    private ArrayList<JSONObject> listaUsuarios = new ArrayList<>();
+    private ArrayList<JSONObject> listaCabañas = new ArrayList<>();
+}
