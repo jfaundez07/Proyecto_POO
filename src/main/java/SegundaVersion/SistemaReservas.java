db4d06af02d2187c2a772da0d4db53215e74bef6
jfaundez07 <j.faundez07@ufromail.cl>
2023-10-21 16:47:54 -0300

    Metodos instanciar Objetos y almacenarlos

--- src/main/java/SegundaVersion/SistemaReservas.java
+++ src/main/java/SegundaVersion/SistemaReservas.java
@@ -1,5 +1,7 @@
 package SegundaVersion;
 
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
 public class SistemaReservas {
@@ -7,4 +9,22 @@ public class SistemaReservas {
     //Listas para almacenar los datos de los usuarios y las cabañas en formato de sus respectivas Clases:
     private ArrayList<Cliente> listaClientes = new ArrayList<>();
     private ArrayList<Cabaña> listaCabañas = new ArrayList<>();
+
+   private Cliente instanciarCliente (JSONObject archivoCliente) {
+       return new Cliente(archivoCliente.getString("Usuario"), archivoCliente.getString("Contraseña"), archivoCliente.getInt("Celular"));
+   }
+
+   private Cabaña instanciarCabaña (JSONObject archivoCabaña) {
+       return new Cabaña(archivoCabaña.getInt("Id"), archivoCabaña.getString("Nombre"), archivoCabaña.getInt("Habitaciones"), archivoCabaña.getInt("Baños"), archivoCabaña.getBoolean("isOcupada"), archivoCabaña.getInt("Arrendatario"));
+   }
+
+   private void almacenarCliente(Cliente nuevoCliente){
+       this.listaClientes.add(nuevoCliente);
+   }
+
+   private void almacenarCabaña(Cabaña nuevaCabaña){
+       this.listaCabañas.add(nuevaCabaña);
+   }
+
+
 }
