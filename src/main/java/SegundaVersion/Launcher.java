e3900031c301d384a8f9925f9def839ffdf1038c
chris <c.gajardo08@ufromail.cl>
2023-10-22 18:24:25 -0300

    Launcher normal

--- src/main/java/SegundaVersion/Launcher.java
+++ src/main/java/SegundaVersion/Launcher.java
@@ -2,13 +2,7 @@ package SegundaVersion;
 
 public class Launcher {
     public static void main(String[] args) {
-        SistemaReservas s = new SistemaReservas();
-        for(Cliente c : s.getListaClientes()){
-            System.out.println(c.getUsuario());
-        }
-        for(Cabaña b : s.getListaCabañas()){
-            System.out.println(b.getArrendatario());
-            System.out.println(b);
-        }
+        Menu lanzar = new Menu();
+        lanzar.MenuBienvenida();
     }
 }
