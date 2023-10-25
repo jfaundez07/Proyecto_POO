3fed43e8cfdc43ca03ede9ded9f5829e4f26b2e8
jfaundez07 <j.faundez07@ufromail.cl>
2023-10-25 10:00:11 -0300

    Atributos de Menu: GestorDeCabañas/Clientes

--- src/main/java/SegundaVersion/Menu.java
+++ src/main/java/SegundaVersion/Menu.java
@@ -4,20 +4,19 @@ import java.util.Scanner;
 
 public class Menu {
     public static Scanner leer = new Scanner(System.in);
+    private GestorDeClientes gestorDeClientes = new GestorDeClientes();
+    private GestorDeCabañas gestorDeCabañas =  new GestorDeCabañas();
 
-    public void MenuBienvenida() {
-
-        GestorDeClientes gestorDeClientes = new GestorDeClientes();
-        GestorDeCabañas gestorDeCabañas = new GestorDeCabañas();
+    public void menuBienvenida() {
 
         String Seleccion;
 
         do {
-            OpcionesMenuBienvenida();
+            opcionesMenuBienvenida();
             Seleccion = leer.nextLine();
 
             switch (Seleccion){
-                case "1" -> menuPrincipal(gestorDeClientes.loginUsario(), gestorDeCabañas);
+                case "1" -> menuPrincipal(gestorDeClientes.loginUsario());
                 case "2" -> gestorDeClientes.singUP();
             }
 
@@ -27,7 +26,7 @@ public class Menu {
         gestorDeClientes.registrarClientesEnArchivoJson();
     }
 
-    public void menuPrincipal(Cliente usuario, GestorDeCabañas gestorDeCabañas) {;
+    public void menuPrincipal(Cliente usuario) {
 
         String seleccion;
 
@@ -50,7 +49,7 @@ public class Menu {
         System.out.println("[0] Salir\n[1] Mostrar Cabañas existentes\n[2] Reservar Cabaña\n[3] Ver Mis Cabañas Reservadas\n[4] Realizar Check-Out\nQue desea hacer?: ");
     }
 
-    public void OpcionesMenuBienvenida() {
+    public void opcionesMenuBienvenida() {
         System.out.println("\n#-----Bienvenido a R.A-----#\nElija una opcion:");
         System.out.println("[0] Salir\n[1] Iniciar sesión\n[2] Registrar usuario\nQue desea hacer?: ");
     }
